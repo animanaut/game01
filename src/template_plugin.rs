61ab2190fb7fc5482d3bf57f89321aa2782c95f9
danyrey <danyrey@gmx.net>
2025-08-19 16:25:54 +0200

    updated template

--- src/template_plugin.rs
+++ src/template_plugin.rs
@@ -1,6 +1,6 @@
 use bevy::app::Plugin;
 
-use AppState::MainMenu;
+use AppState::Running;
 use bevy::prelude::*;
 
 use crate::app_states::AppState;
@@ -13,9 +13,9 @@ pub struct TemplatePlugin;
 
 impl Plugin for TemplatePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(MainMenu), start_template_system)
-            .add_systems(Update, (update_template_system).run_if(in_state(MainMenu)))
-            .add_systems(OnExit(MainMenu), stop_template_system);
+        app.add_systems(OnEnter(Running), start_template_system)
+            .add_systems(Update, (update_template_system).run_if(in_state(Running)))
+            .add_systems(OnExit(Running), stop_template_system);
     }
 }
 
@@ -29,9 +29,11 @@ impl Plugin for TemplatePlugin {
 fn start_template_system(mut _commands: Commands) {
     debug!("starting {}", NAME);
 }
+
 fn update_template_system() {
     debug!("updating {}", NAME);
 }
+
 fn stop_template_system(mut _commands: Commands) {
     debug!("stopping {}", NAME);
 }
@@ -43,23 +45,24 @@ fn stop_template_system(mut _commands: Commands) {
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     //use super::*;
+    //use std::borrow::BorrowMut;
 
     /*
     #[test]
     fn should_test_something() {
         // given
-        //let mut _app = App::new();
+        //let mut app = App::new();
 
         // when
         //app.add_event::<HealthDamageReceived>();
         //app.add_systems(Update, damage_received_listener);
-        //let entity = app.borrow_mut().world.spawn(Health(100)).id();
-        //app.borrow_mut().world.resource_mut::<Events<HealthDamageReceived>>().send(HealthDamageReceived { entity, damage: 10 });
+        //let entity = app.borrow_mut().world().spawn(Health(100)).id();
+        //app.borrow_mut().world().resource_mut::<Events<HealthDamageReceived>>().send(HealthDamageReceived { entity, damage: 10 });
         //app.update();
 
         // then
-        //assert!(app.world.get::<Health>(entity).is_some());
-        //assert_eq!(app.world.get::<Health>(entity).unwrap().0, 90);
+        //assert!(app.world().get::<Health>(entity).is_some());
+        //assert_eq!(app.world().get::<Health>(entity).unwrap().0, 90);
     }
     */
 }
